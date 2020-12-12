ec0a9b9ba1d24ec24b7fd05159be5b587d5951cb
Cesar <rodriguezcesar1214@gmail.com>
2020-12-12 08:02:42 +0000

    skew small enough for 1 clock

---
+++ sreg.cmd
@@ -0,0 +1,22 @@
+h Vdd!
+l GND!
+vector CLOCK clk ~clk
+clock CLOCK 01 10
+ana D q0 q1 q2 q3
+vector in D
+c
+setvector in 0
+c
+setvector in 1
+c
+setvector in 0
+c
+setvector in 1
+c
+setvector in 0
+c
+setvector in 1
+c
+setvector in 0
+c
+setvector in 1
