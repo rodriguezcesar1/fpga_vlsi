db3e61a335dc1cb5ec63751d5f475b65fa9180e3
Kasey Hancock <kasey.hancock@yale.edu>
2020-12-18 15:12:41 -0500

    test script for programming of 1x1 and an update of the 1x1 with different labels

---
+++ 1x1_p_test.cmd
@@ -0,0 +1,77 @@
+h Vdd! p_clk_b 
+l GND! p_clk in
+ana 0 1 2 3 4 5 6 7 8 9 10 11 CB1 CB2 select0 select1 select2 select_out CB3 CB4 A B C D E F G H LB_in LB_out in p_clk
+vector CLOCK p_clk p_clk_b
+clock CLOCK 01 10
+c
+h in
+c
+l in
+c
+h in
+c
+l in
+c
+h in
+c
+l in
+c
+h in
+c
+c
+l in
+c
+c
+c
+c
+c
+h in
+c
+l in
+c
+c
+h in
+c
+l in
+c
+c
+h A
+l E
+s
+h LB_out
+s
+l LB_out
+s
+h CB4 
+l CB3
+s
+l A
+s
+h E CB2
+l CB1
+s
+h A 1
+s
+l A 1
+h 4
+s
+h 3 A
+l 4
+s
+l 3 A
+h 0
+s
+l CB3 CB4
+h 7
+s
+l 7 E
+h 9
+s
+h 10 E
+l 9
+s
+l E 10
+h 6
+s
+
+
