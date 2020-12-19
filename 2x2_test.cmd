3cefac0f733dd25cc37d8c9ff39b7ddb4c41be9e
Kasey Hancock <kasey.hancock@yale.edu>
2020-12-18 21:00:35 -0500

    test script for 2x2 and a 2x2 block without local labels

---
+++ 2x2_test.cmd
@@ -0,0 +1,134 @@
+h Vdd! p_clk_b f_clk_b p_in
+l GND! p_clk f_clk in
+ana in 2 3 4 5 6 7 8 out p_clk f_clk p_in
+vector CLOCK p_clk p_clk_b
+clock CLOCK 01 10
+c
+l p_in
+c
+c
+c
+c
+c
+c
+h p_in
+c
+l p_in
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+h p_in
+c
+l p_in
+c
+h p_in
+c
+l p_in
+c
+c
+c
+c
+h p_in
+c
+l p_in
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+h p_in
+c
+l p_in
+c
+c
+c
+c
+c
+c
+h p_in
+c
+l p_in
+c
+c
+c
+h p_in
+c
+l p_in
+c
+c
+c
+c
+c
+c
+c
+h p_in
+c
+l p_in
+c
+h p_in
+c
+l p_in
+c
+c
+c
+c
+c
+h p_in
+c
+l p_in
+c
+h p_in
+c
+l p_in
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+l p_clk f_clk_b
+h p_clk_b f_clk
+s
+h f_clk_b
+l f_clk
+s
+h f_clk
+l f_clk_b
+s
+h f_clk_b
+l f_clk
+s
+h f_clk
+l f_clk_b
+s
+h f_clk_b
+l f_clk
+s
+h f_clk
+l f_clk_b
+s
+h f_clk_b
+l f_clk
+s
+h f_clk
+l f_clk_b
