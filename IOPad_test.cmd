238f22fac261c47a9b5925bd402f247f62bc3135
Cesar <rodriguezcesar1214@gmail.com>
2020-12-19 04:23:15 +0000

    fpga

---
+++ IOPad_test.cmd
@@ -0,0 +1,171 @@
+h Vdd!
+l GND!
+ana program data mode rst_b out
+vector CLOCK clk
+clock CLOCK 0 1
+c
+l rst_b
+c
+h rst_b
+l mode
+c
+l program
+c
+c
+c
+c
+c
+c
+h program
+c
+l program
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+h program
+c
+l program
+c
+h program
+c
+l program
+c
+c
+c
+c
+h program
+c
+l program
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+h program
+c
+l program
+c
+c
+c
+c
+c
+c
+h program
+c
+l program
+c
+c
+c
+h program
+c
+l program
+c
+c
+c
+c
+c
+c
+c
+h program
+c
+l program
+c
+h program
+c
+l program
+c
+c
+c
+c
+c
+h program
+c
+l program
+c
+h program
+c
+l program
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+h mode
+c
+h data
+c
+c
+c
+c
+l data
+c
+c
+c
+c
+c
+h data
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+l data
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
+c
